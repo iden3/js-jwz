5432ff8b146d9670692ebea61794486e6ab9dada
GopherDID <74898029+vmidyllic@users.noreply.github.com>
2025-04-29 23:04:53 +0300

    Bump/core 1.6.0 (#45)
    * bump core
    
    * 1.9.0
    
    * fix rollup

--- config/rollup.config.mjs
+++ config/rollup.config.mjs
@@ -1,8 +1,8 @@
 import commonJS from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import tsConfig from '../tsconfig.json' assert { type: 'json' };
-import packageJson from '../package.json' assert { type: 'json' };
+import tsConfig from '../tsconfig.json' with { type: 'json' };
+import packageJson from '../package.json' with { type: 'json' };
 import terser from '@rollup/plugin-terser';
 const external = [
   ...Object.keys(packageJson.peerDependencies).filter((key) => key.startsWith('@iden3/')),
