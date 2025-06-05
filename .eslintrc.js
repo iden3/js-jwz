8a5c53876e7fe1b490595c8c272940362608866d
Kolezhniuk <dmytro.k@privado.id>
2025-06-05 21:42:37 +0800

    add word to ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
         ...spellcheckerRule,
         cspell: {
           ...cspellConfig,
-          ignoreWords: ['mymessage']
+          ignoreWords: ['mymessage', 'alphabeta']
         }
       }
     ]
