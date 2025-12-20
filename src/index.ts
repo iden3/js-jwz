629ba0765024f136b4b7d76a64ac05fd09284bc2
GopherDID <vladcisco.vm@gmail.com>
2025-12-20 22:32:26 +0200

    return back (#52)

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,10 @@ import {
 import { provingMethodGroth16AuthV2Instance } from './authV2Groth16';
 import { Groth16VerificationKey, verifyGroth16Proof } from './common';
 import { witnessBuilder } from './witness_calculator';
-import { provingMethodGroth16AuthV3Instance } from './authV3Groth16';
+import {
+  provingMethodGroth16AuthV3Instance,
+  provingMethodGroth16AuthV3_8_32Instance
+} from './authV3Groth16';
 
 registerProvingMethod(
   provingMethodGroth16AuthV2Instance.methodAlg,
@@ -25,11 +28,17 @@ registerProvingMethod(
   () => provingMethodGroth16AuthV3Instance
 );
 
+registerProvingMethod(
+  provingMethodGroth16AuthV3_8_32Instance.methodAlg,
+  () => provingMethodGroth16AuthV3_8_32Instance
+);
+
 const proving = {
   registerProvingMethod,
   getProvingMethod,
   provingMethodGroth16AuthV2Instance,
-  provingMethodGroth16AuthV3Instance
+  provingMethodGroth16AuthV3Instance,
+  provingMethodGroth16AuthV3_8_32Instance
 };
 
 export {
